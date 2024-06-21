a853617af7634c75eaeee07416031c414cfd9818
Louis Gombert <louisgombert@gmail.com>
2024-06-21 21:25:59 +0200

    Windows SDL2 compiles

--- renderer/dyna_gl.h
+++ renderer/dyna_gl.h
@@ -217,7 +217,6 @@ DYNAEXTERN(glBlitFramebufferEXT_fp, dglBlitFramebufferEXT);
 #ifdef DECLARE_OPENGL
 static module OpenGLDLLInst;
 
-#ifdef __LINUX__
 static void *__SDL_mod_GetSymbol(const char *funcStr) {
   void *retVal = NULL;
 
@@ -237,8 +236,6 @@ static void *__SDL_mod_GetSymbol(const char *funcStr) {
 /****************** WARNING: NASTY HACK! ***********************/
 #define mod_GetSymbol(x, funcStr, y) __SDL_mod_GetSymbol(funcStr)
 /****************** WARNING: NASTY HACK! ***********************/
-
-#endif
 
 #ifdef __LINUX__
 extern char *__orig_pwd;
